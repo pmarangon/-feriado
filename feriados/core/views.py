bdcac62385b8f9967fc841e9103f6c86a058b44a
Paula Masson Marangon <56054345+pmarangon@users.noreply.github.com>
2023-04-02 10:03:03 -0300

    adiciona feriado Tiradentes

--- feriados/core/views.py
+++ feriados/core/views.py
@@ -3,3 +3,6 @@ def natal(request):
  contexto = {'natal': True,
  'carnaval': False}
  return render(request, 'natal.html', contexto)
+def tiradentes(request):
+  contexto = {'tiradentes':True, 'pascoa':False}
+  return render(request,'tiradentes.html', contexto)
